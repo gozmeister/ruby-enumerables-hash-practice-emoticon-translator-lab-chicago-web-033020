d59083e3bdf6309fc1d1c799546b32977bbd831e
Sam Gorick <samgorick.sg@gmail.com>
2020-03-19 22:56:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,9 +21,10 @@ def get_japanese_emoticon(file_path, emoticon)
   file = load_library(file_path)
   file[:get_emoticon].select do |key, value|
       key == emoticon
-      return value
+      result = value
       #binding.pry
     end
+    result
 end
 
 def get_english_meaning(file_path, emoticon)
