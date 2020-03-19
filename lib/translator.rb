9766683e25bbd15b0b226064ca3af4ef7dd217f4
Sam Gorick <samgorick.sg@gmail.com>
2020-03-19 22:59:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return value
     end
     end
+   return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
